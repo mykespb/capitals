3ea3639ee02a0319bc5f1e6d13395c99e58b4d12
mykespb <mykespb@gmail.com>
2019-05-26 02:09:05 +0300

    good json data read, processes and printed

--- capital/djson/cap1/views.py
+++ capital/djson/cap1/views.py
@@ -2,13 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_protect, csrf_exempt
 
+import json
+
 from .models import Category
 
 @csrf_exempt
 def categories (request):
 	if request.method == 'POST':
+		data = json.load(request)
 #		return HttpResponse("we got POST")
-		return JsonResponse ({'method': 'POST', 'foo': 'bar'})
+#		return JsonResponse ({'method': 'POST', 'foo': 'bar'})
+		return JsonResponse (data = data)
 
 	else:
 #		return HttpResponse("we got GET")
