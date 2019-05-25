35f631b4ceae43d0e2e81ae9e34be22f5fbaae5b
mykespb <mykespb@gmail.com>
2019-05-26 02:24:45 +0300

    better names, calls and parameters

--- capital/djson/cap1/urls.py
+++ capital/djson/cap1/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path ('categories/<num>/', views.categories, name="categories"),
     path ('categories/', views.categories, name="categories"),
 ]
--- capital/djson/cap1/views.py
+++ capital/djson/cap1/views.py
@@ -1,19 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_protect, csrf_exempt
+from django.views.decorators.csrf import csrf_exempt
 
 import json
 
 from .models import Category
 
 @csrf_exempt
-def categories (request):
+def categories (request, num=0):
 	if request.method == 'POST':
 		data = json.load(request)
 #		return HttpResponse("we got POST")
 #		return JsonResponse ({'method': 'POST', 'foo': 'bar'})
-		return JsonResponse (data = data)
+#		return JsonResponse (data = data)
+		return JsonResponse (data)
 
 	else:
 #		return HttpResponse("we got GET")
-		return JsonResponse ({'method': 'GET', 'foo': 'bar'})
+		return JsonResponse ({'method': 'GET', 'num': num})
